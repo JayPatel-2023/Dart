b1520ec09fd113f40d4ae7713d10a89531bca550
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-02-23 13:26:10 +0530

    logical operators

--- new.dart
+++ new.dart
@@ -59,7 +59,8 @@ void main() {
   print("sum with isNaN = ${(i + j).isNaN}");
   print("sum with isNegative = ${(i + j).isNegative}");
   print("sum with runtimeType = ${(i + j).runtimeType}");
-  print("next all the operations is accepts the other methos like hashcode which is given above!!!");
+  print(
+      "next all the operations is accepts the other methos like hashcode which is given above!!!");
   print("sum with abs() = ${(i + j).abs()}");
   print("sum with ceil() = ${(i + j).ceil()}");
   print("sum with ceilToDouble() = ${(i + j).ceilToDouble()}");
@@ -96,13 +97,24 @@ void main() {
   //relational operators
   double l = 5.67;
   double m = 7;
-  print("equal == : ${l==m}");
-  print("not equal != : ${l!=m}");
+  print("equal == : ${l == m}");
+  print("not equal != : ${l != m}");
   //all operations which are apply on arithmetic operators is also apply on unary operators
   print("greater than > : ${l > m}");
-  print("less than < : ${(l<m)}");
+  print("less than < : ${(l < m)}");
   print("greater than or equal >= : ${l >= m}");
   print("less than or equal <= : ${l <= m}");
 
+  //logical operators
+  bool N = true;
+  bool O = false;
+  print("true && true : ${N && N}");
+  print("true && false : ${N && O}");
+  print("false && true : ${O && N}");
+  print("false && false : ${O && O}");
+  print("true || true : ${N || N}");
+  print("true || false : ${N || O}");
+  print("false || true : ${O || N}");
+  print("false || false : ${O || O}");
   
 }
