5aa07b896963b7637ff2d1eef43d4adc5a22c17f
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-03-01 21:16:07 +0530

    type test complete

--- new.dart
+++ new.dart
@@ -29,16 +29,7 @@ void main() {
   print("?: => ${N == true ? true : false}");
 
   //type test operators
-  int a = 10;
-  double b = 10.5;
-  String c = 'abcdefghij';
-  bool d = true;
-  var e = 100;
-  print("chech var type is int : ${a is int}");
-  print("chech var type is double : ${b is double}");
-  print("chech var type is string : ${c is String}");
-  print("chech var type is bool : ${d is bool}");
-  print("chech var type is var : ${e is int}");
+  
 
   //assignment operators
   double p = 10.37;
---
+++ type_test.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  int a = 10;
+  double b = 10.5;
+  String c = 'abcdefghij';
+  bool d = true;
+  var e = 100;
+  print("chech var type is int : ${a is int}");
+  print("chech var type is double : ${b is double}");
+  print("chech var type is string : ${c is String}");
+  print("chech var type is bool : ${d is bool}");
+  print("chech var type is var : ${e is int}");
+}
