fdb2b88f22c7cd186185f69875af3ff7d2cee809
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-03-01 21:09:26 +0530

    unary operator complete

--- new.dart
+++ new.dart
@@ -20,18 +20,7 @@ void main() {
   /// operators
   
   //unary operators
-  //all operations which are apply on arithmetic operators is also apply on unary operators
-  double k = 5.67;
-  print("k : $k");
-  print("k++ : ${k++}");
-  print("k : $k");
-  print("k-- : ${k--}");
-  print("k : $k");
-  print("++k : ${++k}");
-  print("k : $k");
-  print("--k : ${--k}");
-  print("k : $k");
-
+  
   //relational operators
   double l = 5.67;
   double m = 7;
---
+++ unary_op.dart
@@ -0,0 +1,14 @@
+void main() {
+  //all operations which are apply on arithmetic operators is also apply on unary operators
+  double k = 5.67;
+  print("k : $k");
+  print("k++ : ${k++}");
+  print("k : $k");
+  print("k-- : ${k--}");
+  print("k : $k");
+  print("++k : ${++k}");
+  print("k : $k");
+  print("--k : ${--k}");
+  print("k : $k");
+
+}
