c60e316cbc29a43165735e88f417e334e378782f
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-03-01 21:19:26 +0530

    assignment operator complete

---
+++ assignment_op.dart
@@ -0,0 +1,9 @@
+void main() {
+  //assignment operators
+  double p = 10.37;
+  //all arithmetic operations can be perform on this all operators like hashcode
+  print("p+=5 : ${(p += 5)}");
+  print("p-=5 : ${(p -= 5)}");
+  print("p*=5 : ${(p *= 5)}");
+  print("p/=5 : ${(p /= 5)}");
+}
--- new.dart
+++ new.dart
@@ -29,13 +29,7 @@ void main() {
   //type test operators
   
 
-  //assignment operators
-  double p = 10.37;
-  //all arithmetic operations can be perform on this all operators like hashcode
-  print("p+=5 : ${(p += 5)}");
-  print("p-=5 : ${(p -= 5)}");
-  print("p*=5 : ${(p *= 5)}");
-  print("p/=5 : ${(p /= 5)}");
+  
 
   
   
