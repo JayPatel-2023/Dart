72c21edd4ce3587760007bff8a6c1b5f2e165b47
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-03-01 21:10:31 +0530

    relational operator complete

--- new.dart
+++ new.dart
@@ -21,17 +21,7 @@ void main() {
   
   //unary operators
   
-  //relational operators
-  double l = 5.67;
-  double m = 7;
-  print("equal == : ${l == m}");
-  print("not equal != : ${l != m}");
-  //all operations which are apply on arithmetic operators is also apply on unary operators
-  print("greater than > : ${l > m}");
-  print("less than < : ${(l < m)}");
-  print("greater than or equal >= : ${l >= m}");
-  print("less than or equal <= : ${l <= m}");
-
+  
   //logical operators
   bool N = true;
   bool O = false;
---
+++ relational_op.dart
@@ -0,0 +1,13 @@
+void main() {
+  //relational operators
+  double l = 5.67;
+  double m = 7;
+  print("equal == : ${l == m}");
+  print("not equal != : ${l != m}");
+  //all operations which are apply on arithmetic operators is also apply on unary operators
+  print("greater than > : ${l > m}");
+  print("less than < : ${(l < m)}");
+  print("greater than or equal >= : ${l >= m}");
+  print("less than or equal <= : ${l <= m}");
+
+}
