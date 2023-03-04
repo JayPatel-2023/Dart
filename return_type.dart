01390df93e72d0358bcc58ecba590772d6bbdce7
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-03-04 11:08:18 +0530

    main function with different datatype

---
+++ return_type.dart
@@ -0,0 +1,22 @@
+void main() {
+  print(Fun_int());
+  print(Fun_string());
+  print(Fun_bool());
+  print(Fun_double());
+}
+
+int Fun_int() {
+  return 5;
+}
+
+String Fun_string() {
+  return "Hello World!!!";
+}
+
+bool Fun_bool() {
+  return true;
+}
+
+double Fun_double() {
+  return 5.6;
+}
