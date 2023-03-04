d8399bf1d0ef6e02dd15c143c42b29768942910e
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-03-04 18:53:27 +0530

    user input updated

--- Basic/user_input.dart
+++ Basic/user_input.dart
@@ -2,6 +2,18 @@ import 'dart:io';
 
 void main() {
   print("Enter name:");
-  String? name  = stdin.readLineSync();
+  String? name = stdin.readLineSync();
   print("The entered name is ${name}");
+
+//Integer value input
+  print("Enter a number : ");
+  int a = int.parse(stdin.readLineSync()!);
+  print("Entered number is $a.");
+
+//Floating point value input
+  print("Enter a number : ");
+  double b = double.parse(stdin.readLineSync()!);
+  print("Entered number is $b.");
+
+ 
 }
