4e908c74f3fd986c9ca9535939ffdc4bd1f59b7f
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-03-06 10:29:18 +0530

    raw string and special character added in datatype

--- Basic/datatype.dart
+++ Basic/datatype.dart
@@ -36,6 +36,19 @@ void main() {
   print("Multiline text is $multiLineText");
   print("Other multiline text is $otherMultiLineText");
 
+  ///special characters in string
+  // Using \n and \t   
+  print("I am from \nUS.");
+  print("I am from \tUS.");
+
+  ///raw string 
+  num prize = 10;
+  String withoutRawString = "The value of prize is \t $prize"; // regular String
+  String withRawString =r"The value of prize is \t $prize"; // raw String
+
+  print("Without Raw: $withoutRawString"); // regular result
+  print("With Raw: $withRawString"); // with raw result
+
   bool d = true;
   print("d type = ${d.runtimeType} and value is $d.");
   print("all var variable declartion !!!");
