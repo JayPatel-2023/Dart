f9560956e9bc27e76f90ce14d7a89080757ae642
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-03-06 10:25:44 +0530

    add multiline string in datatype

--- Basic/datatype.dart
+++ Basic/datatype.dart
@@ -1,24 +1,41 @@
 void main() {
-
   //Built in datatypes
   //1.Numbers(int,double,num(int + double))
-  int a = 10;          //integer values
+  int a = 10; //integer values
   print("a type = ${a.runtimeType} and value is $a");
-  
-  double b = 10.5;     //floating values
+
+  double b = 10.5; //floating values
   print("b type = ${b.runtimeType} and value is $b");
-  
-  num ab = 10;         //assigned integer & floating values
+
+  num ab = 10; //assigned integer & floating values
   print("ab type = ${ab.runtimeType} and value is $ab");
   ab = 10.5;
   print("ab type = ${ab.runtimeType} and value is $ab");
-  
+
   // string declare with
   // '' : for small words
   // "" : for single line statement
   // """""" : for multiline statement
   String c = 'abcdefghij';
   print("c type = ${c.runtimeType} and value is $c.");
+  // Multi Line Using Single Quotes   
+  String multiLineText = '''
+  This is Multi Line Text
+  with 3 single quote
+  I am also writing here.
+  ''';
+    
+  // Multi Line Using Double Quotes   
+  String otherMultiLineText = """
+  This is Multi Line Text
+  with 3 double quote
+  I am also writing here.
+  """;
+    
+  // Printing Information   
+  print("Multiline text is $multiLineText");
+  print("Other multiline text is $otherMultiLineText");
+
   bool d = true;
   print("d type = ${d.runtimeType} and value is $d.");
   print("all var variable declartion !!!");
---
+++ Packages/core/double.dart
@@ -0,0 +1,18 @@
+void main() {
+
+  
+  print("\n\n");
+  print("-----extra-----");
+  var exponents = 1.42e5;
+  print(exponents);
+  double dd = double.infinity;
+  print(dd * 5);
+  dd = double.maxFinite;
+  print(dd);
+  dd = double.minPositive;
+  print(dd);
+  dd = double.nan;
+  print(dd);
+  dd = double.negativeInfinity;
+  print(dd);
+}
