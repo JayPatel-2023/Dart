9f6a4b6892c5d40a0a8c1bb1ecb627a479f0c622
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-03-07 20:46:59 +0530

    relational operator operations perform

--- Basic/relational_op.dart
+++ Basic/relational_op.dart
@@ -2,12 +2,23 @@ void main() {
   //relational operators
   double l = 5.67;
   double m = 7;
-  print("equal == : ${l == m}");
-  print("not equal != : ${l != m}");
-  //all operations which are apply on arithmetic operators is also apply on unary operators
-  print("greater than > : ${l > m}");
-  print("less than < : ${(l < m)}");
-  print("greater than or equal >= : ${l >= m}");
-  print("less than or equal <= : ${l <= m}");
+
+  print("equal (l == m): ${l == m}");
+  print("not equal (l != m) : ${l != m}");
+  print("greater than (l > m) : ${l > m}");
+  print("less than (l < m) : ${(l < m)}");
+  print("greater than or equal (l >= m) : ${l >= m}");
+  print("less than or equal (l <= m) : ${l <= m}");
+
+  //operations on realtional operators
+  print("hashcode for equal (l == m): ${(l == m).hashCode}");
+  print("hashcode for not equal (l != m) : ${(l != m).hashCode}");
+  print("hashcode for greater than (l > m) : ${(l > m).hashCode}");
+  print("hashcode for less than (l < m) : ${(l < m).hashCode}");
+  print("hashcode for greater than or equal (l >= m) : ${(l >= m).hashCode}");
+  print("hashcode for less than or equal (l <= m) : ${(l <= m).hashCode}");
+
+  //here you can not perform any properties which perform on arithmetic operator
+  //because here all results give either true or false not any number 
 
 }
