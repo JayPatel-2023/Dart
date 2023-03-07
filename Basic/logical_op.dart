b009c1e743351faee1e434cfa16e3b57f63e07df
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-03-07 21:05:08 +0530

    logical operator done

--- Basic/logical_op.dart
+++ Basic/logical_op.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: dead_code
 
 void main() {
-   //logical operators
+  //logical operators
   bool N = true;
   bool O = false;
+  print("N : $N");
+  print("O : $O");
   print("true && true : ${N && N}");
   print("true && false : ${N && O}");
   print("false && true : ${O && N}");
@@ -12,7 +14,9 @@ void main() {
   print("true || false : ${N || O}");
   print("false || true : ${O || N}");
   print("false || false : ${O || O}");
+  print("!N : ${(!N)}");
+  print("!O : ${(!O)}");
 
-  //also do this for all 
+  //also do this for all
   print("Not(true && true) : ${!(N && N)}");
 }
