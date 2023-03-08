c23d1f868fbcc83cc9ebf9c90764d3aab56f59d2
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-03-08 21:40:51 +0530

    if statement

---
+++ Conditions/if.dart
@@ -0,0 +1,15 @@
+void main() {
+  ///if the condition satisfies than it will go into the { } brackets.
+  ///condition is not satisfies than skip the if block execute after the if block.
+  ///
+  bool b = true;
+  if (b) {
+    print("condition satisfied!!!");
+  }
+
+  int age = 18;
+  if (age > 18) {
+    print("you eligible for voting.");
+  }
+  print("age condititon not satisfied.");
+}
